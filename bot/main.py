fbf6de5b7cf173c299520373cb18337b8fccebca
thrns666 <super.tawer@gmail.com>
2024-01-05 15:58:28 +0300

    init

---
+++ bot/main.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher
+from aiogram.dispatcher.filters import Command, CommandStart, Text
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove, BotCommand
+import config
+
+# Bot init
+
+
